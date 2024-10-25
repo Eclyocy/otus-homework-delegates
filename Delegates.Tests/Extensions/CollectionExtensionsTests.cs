607cc9c1d7df3d62e2a1691b6521002c3694cc90
Ksenia Altbregen <ksanph@gmail.com>
2024-10-25 23:55:30 +0300

    feat: add GetMax collection extension

---
+++ Delegates.Tests/Extensions/CollectionExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using Delegates.Extensions;
+using NUnit.Framework;
+
+namespace Delegates.Tests
+{
+    [TestFixture]
+    public class CollectionExtensionsTests
+    {
+        [Test]
+        public void GetMax_ThrowsArgumentNullException_WhenStringCollectionIsNull()
+        {
+            // Arrange
+            List<string> collection = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => collection.GetMax(float.Parse));
+        }
+
+        [Test]
+        public void GetMax_ReturnsNull_WhenStringCollectionIsEmpty()
+        {
+            // Arrange
+            List<string> collection = [];
+
+            // Act
+            string? maxItem = collection.GetMax(float.Parse);
+
+            // Assert
+            Assert.That(maxItem, Is.Null);
+        }
+
+        [Test]
+        public void GetMax_ReturnsMax_WhenStringCollectionIsNotEmpty()
+        {
+            // Arrange
+            List<string> collection = [ "-5", "5", "10", "5", "2", "-10" ];
+
+            // Act
+            string? maxItem = collection.GetMax(float.Parse);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(maxItem, Is.Not.Null);
+                Assert.That(maxItem?.GetType(), Is.EqualTo(typeof(string)));
+                Assert.That(maxItem, Is.EqualTo("10"));
+            });
+        }
+    }
+}
---
+++ Delegates/Extensions/CollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+namespace Delegates.Extensions
+{
+    /// <summary>
+    /// Extensions for <see cref="IEnumerable"/> collections.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Returns the maximum element of the <paramref name="collection"/>.
+        /// Uses <paramref name="convertToNumber"/> delegate to convert collection elements to numbers.
+        /// </summary>
+        /// <returns>
+        /// The maximum element of the collection, or null for an empty collection.
+        /// </returns>
+        /// <typeparam name="T">Type of collection elements.</typeparam>
+        /// <exception cref="ArgumentNullException">Thrown in case the collection is null.</exception>
+        public static T? GetMax<T>(
+            this IEnumerable<T> collection,
+            Func<T, float> convertToNumber)
+            where T : class
+        {
+            ArgumentNullException.ThrowIfNull(collection);
+
+            if (!collection.Any())
+            {
+                return null;
+            }
+
+            T? maxItem = null;
+
+            float maxItemNumber = float.MinValue;
+            foreach (T item in collection)
+            {
+                float itemNumber = convertToNumber(item);
+
+                if (itemNumber > maxItemNumber)
+                {
+                    maxItemNumber = itemNumber;
+                    maxItem = item;
+                }
+            }
+
+            return maxItem;
+        }
+    }
+}
